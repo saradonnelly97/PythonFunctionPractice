30050c1c3aa88f15b7dc56c9b0faa91185684a9c
saradonnelly97 <sara.donnelly8@gmail.com>
2023-10-20 16:23:06 -0400

    pascal function

--- pascal.py
+++ pascal.py
@@ -1,2 +1,12 @@
 #Prompt 5
-def pascal()
+def pascal(n):
+    for line in range(n):
+        current_val = 1 
+        for i in range(line + 1):
+            print(current_val, end = " ")
+
+            current_val = current_val * (line - 1) // (i + 1)
+    print()
+
+n = 5
+pascal(2)
